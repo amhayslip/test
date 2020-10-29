2748e8d862c925d1179fca5e3c7dc64032b27298
Aaron Hayslip <amhayslip@gmail.com>
2020-10-29 11:32:22 -0400

    Update submission.js

--- submission.js
+++ submission.js
@@ -44,3 +44,35 @@ const findFrequency = function(array) {
     least: minItem,
   };
 };
+
+const scoreScrabble = function(str) {
+  const lowerCaseStr = str.toLowerCase();
+
+  const scores = {
+    1: ['a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't'],
+    2: ['d', 'g'],
+    3: ['b', 'c', 'm', 'p'],
+    4: ['f', 'h', 'v', 'w', 'y'],
+    5: ['k'],
+    8: ['j', 'x'],
+    10: ['q', 'z'],
+  };
+
+  let score = 0;
+
+  for (let i = 0; i < lowerCaseStr.length; i++) {
+    const strLetter = lowerCaseStr[i];
+
+    for (prop in scores) {
+      const lettArr = scores[prop];
+
+      lettArr.forEach(function(letter) {
+        if (strLetter === letter) {
+          score += parseInt(prop);
+        }
+      });
+    }
+  }
+
+  return score;
+};
